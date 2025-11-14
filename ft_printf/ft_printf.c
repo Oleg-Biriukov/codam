6c2b2f73ceeff4809d95459d111af4e759e70001
OlehLP <you@example.com>
2025-11-14 11:21:27 +0100

    addition

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-static t_list	*sav_offset(t_list *array, char *format)
+static t_list	*sav_offset(t_list *array, const char *format)
 {
 	while (*format != '\0')
 	{
@@ -24,23 +24,48 @@ static void	*fr_ee_all(va_list args, t_list *array)
 	return (NULL);
 }
 
+static void	*convert(int c, va_list args)
+{
+	if (c == 'd')
+		ft_putnbr_fd(va_arg(args, int), 0);
+	else if (c == 's')
+		ft_putstr_fd(va_arg(args, char *), 0);
+	else if (c == 'c')
+		ft_putchar_fd(va_arg(args, int), 0);
+	else if (c == '%')
+		ft_putchar_fd('%', 0);
+	else if (c == 'u')
+		ft_putchar_fd(va_arg(args, unsigned int), 0);
+	else
+		return (NULL);
+	return (args);
+}
+
+static void	ft_putnchar_fd(const char *str, size_t len)
+{
+	while (len--)
+		ft_putchar_fd(*(str++), 0);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	t_list	*array;
 
-	array = sav_offset(NULL, (char *) format);
-	for(size_t i = 0; i < la_len(array); i++)
+	array = sav_offset(NULL, format);
+	va_start(args, format);
+	while (array->next != NULL)
 	{
-		printf("%s\n", (char *) array->content);
+		ft_putnchar_fd((char *) format, array->content - (void *) format);
+		convert(*((char *) array->content + 1), args);
+		format += array->content - (void *) format + 1;
 		array = array->next;
 	}
-	printf("%s\n", format);
 	fr_ee_all(args, array);
 	return (0);
 }
 
 int main()
 {
-	return (ft_printf("1234%%789%d", 56, 10));
+	return (ft_printf("1234%%789%d\n", 56));
 }
