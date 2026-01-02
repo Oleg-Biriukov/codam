faf529d7a3d881e7b39c2cc184806f81795ce315
OlehLP <you@example.com>
2026-01-02 13:47:16 +0100

    python

---
+++ py_module01/ex0/ft_garden_intro.py
@@ -0,0 +1,5 @@
+def main(name: str, height: int, age: int):
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}\nHeight: {height}\nAge: {age}")
+    print("=== End of Program ===")
+
