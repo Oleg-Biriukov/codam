ab2e9ac0857b593bcfeb163168d8d9bb497269f1
OlehLP <you@example.com>
2026-02-06 21:48:56 +0100

    ex0 in py_mod08

--- py_module08/ex0/construct.py
+++ py_module08/ex0/construct.py
@@ -1,8 +1,34 @@
-# import sys, os
+import sys
+import os
+import site
+
+no_in_venv = f'''MATRIX STATUS: You're still plugged in\n
+Current Python: {sys.base_prefix}
+Virtual Environment: None detected
+\nWARNING: You're in the global environment!
+The machines can see everything you install.
+\nTo enter the construct, run:
+python -m venv matrix_env
+source matrix_env/bin/activate # On Unix
+matrix_env
+Scripts
+activate # On Windows'''
+
+in_venv = f'''MATRIX STATUS: Welcome to the construct
+\nCurrent Python: {sys.base_prefix}
+Virtual Environment: {os.environ.get("VIRTUAL_ENV")}
+Environment Path: {sys.prefix}
+\nSUCCESS: You're in an isolated environment!
+Safe to install packages without affecting
+the global system.
+\nPackage installation path:\n{site.getsitepackages()[0]}'''
 
 
 def main():
-    pass
+    if sys.prefix != sys.base_prefix:
+        print(in_venv)
+    else:
+        print(no_in_venv)
 
 
 if __name__ == "__main__":
