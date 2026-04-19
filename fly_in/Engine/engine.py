4c1e264d9fd0b259e8e47ce19bb3eaad5b917580
Oleh Birukov <obirukov@f0r2s7.codam.nl>
2026-04-19 17:16:52 +0200

    adsf

--- fly_in/Engine/engine.py
+++ fly_in/Engine/engine.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, PrivateAttr
 from ConfigCompiler.ConfigCompiler import DataConf, ConfigCompiler
-from typing import List, ClassVar
+from typing import List, ClassVar, Self
 from Engine.strategies import Strategy
 from hubs.hub import Hub, Dron
 import pygame as p
 import sys
 import datetime as dt
+
+
+class Camera(BaseModel):
+        pos: tuple[int, int] = (0, 0)
+        zoom: float = 1.0
 
 
 class Engine(BaseModel):
@@ -17,6 +22,7 @@ class Engine(BaseModel):
             'start_end': (661, 17, 408, 308),
             'dron': (1169, 115, 220, 155)
             })
+    _cmr: Camera = PrivateAttr()
     WIDTH: ClassVar[int] = 1000
     HEIGHT: ClassVar[int] = 1000
 
@@ -28,10 +34,13 @@ class Engine(BaseModel):
                         self._data['dron']))) != 0
 
     def configure(self, filename: str) -> None:
+        self._cmr = Camera()
+
         ConfigCompiler.modify_path(filename)
         print(f'\n[{dt.datetime.now()}] Initialization pygame module')
         p.init()
         print(f'[{dt.datetime.now()}] Loading assets...', end=' ')
+
         try:
             asset = p.image.load('assets/hubs_dron.png')
             print('OK')
@@ -43,34 +52,25 @@ class Engine(BaseModel):
             print('KO')
             print('Shutting down ...')
             sys.exit()
+
         hub_p = asset.subsurface(self._assets['hub']).copy()
         start_end = asset.subsurface(self._assets['start_end']).copy()
         dron_p = asset.subsurface(self._assets['dron']).copy()
+
         self._assets['hub'] = p.transform.scale(hub_p, (100, 100))
         self._assets['start_end'] = p.transform.scale(start_end, (100, 100))
         self._assets['dron'] = p.transform.scale(dron_p, (30, 30))
-        self._screen = p.display.set_mode((Engine.WIDTH, Engine.HEIGHT))
+
+        self._screen = p.display.set_mode((Engine.WIDTH, Engine.HEIGHT),
+                                          p.RESIZABLE)
         self._clock = p.time.Clock()
 
-#         for h in self._data['hubs']:
-#             print(f'''Name: {h.name}
-# Position: {h.pos}
-# Zone: {h.zone}
-# Color: {h.color}
-# Maxium drones: {h.max_drones}''')
-#             for n, c in h.next:
-#                 print(f'''Next of {h.name}`s\n\tName: {n.name}
-# \tPosition: {n.pos}
-# \tZone: {n.zone}
-# \tColor: {n.color}
-# \tMaxium drones: {n.max_drones}
-# \tMaxium connection capicity to {h.name}: {c}
-# ''')
-
-    def make_turn(self) -> bool:
+    def make_turn(self) -> None:
         turns: int = 0
         sv_con_cap: List[list[int]] = [[n for n in hub.next]
                                        for hub in self._data['hubs']]
+        is_running: bool = True
+        zoom: float
 
         def set_to_null() -> None:
             for hub in self._data['hubs']:
@@ -95,21 +95,44 @@ class Engine(BaseModel):
                 dron.route.append((turn+1, route[turn]))
             return True
 
-        while True:
+        while is_running:
+            zoom = self._cmr.zoom
             for event in p.event.get():
                 if event.type == p.QUIT:
-                    break
+                    is_running = False
+                if event.type == p.VIDEORESIZE:
+                    Engine.WIDTH, Engine.HEIGHT = event.w, event.h
+                if event.type == p.MOUSEWHEEL:
+                    zoom += event.y * 0.1
+                    zoom = max(0.2, min(zoom, 5))
+                    self._cmr.zoom = zoom
 
             self._screen.fill((0, 0, 0))
 
             for hb in self._data['hubs']:
+                img: p.Surface
+                r_img: p.Rect
+
                 x, y = hb.pos
-                x = (Engine.WIDTH / 2) + (x * 200)
-                y = (Engine.HEIGHT / 2) + (y * 200)
                 if hb == self._data['end_hub'] or hb == self._data['start_hub']:
-                    self._screen.blit(self._assets['start_end'], (x, y))
+                    self._assets['start_end'] = p.transform.scale(
+                        self._assets['start_end'],
+                        (int(100 * zoom), int(100 * zoom))
+                        )
+
+                    img = self._assets['start_end']
                 else:
-                    self._screen.blit(self._assets['hub'], (x, y))
+                    self._assets['hub'] = p.transform.scale(
+                        self._assets['hub'],
+                        (int(100), int(100))
+                        )
+
+                    img = self._assets['hub']
+
+                r_img = img.get_rect()
+                r_img.center = ((Engine.WIDTH // 2) + (x * int(200 * zoom)),
+                                (Engine.HEIGHT // 2) + (y * int(200 * zoom)))
+                self._screen.blit(img, r_img)
             if self.is_done:
                 print(f'================={turns+1}==================')
                 for h in range(len(self._data['hubs'])):
@@ -123,6 +146,6 @@ class Engine(BaseModel):
                             print(f'd{d.id} -> {d.pos.name}')
                             continue
                     print(f'd{d.id} -> {d.pos.name}')
+                turns += 1
             p.display.flip()
-            turns += 1
-        return True
+        p.quit()
